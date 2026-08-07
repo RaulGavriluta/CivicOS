3de4dff38b4703896f939ee4fa23b1acc0caac3d
Raul Gavriluta <raul.gavriluta7@gmail.com>
2026-08-07 15:54:58 +0300

    chore(api): setup Spring Boot with PostgreSQL

---
+++ apps/api/src/test/java/com/civicos/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civicos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
